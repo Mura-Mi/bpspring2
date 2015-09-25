65bbe08227670f44208b9bbad7c8ed21d52e0c8c
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2015-09-26 01:59:34 +0900

    Show place name in place show window

--- spec/views/places/show.html.erb_spec.rb
+++ spec/views/places/show.html.erb_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "places/show", type: :view do
   before(:each) do
-    @place = assign(:place, Place.create!())
+    @place = assign(:place, Place.create!({ name: "Tokyo Cul Cul" }))
   end
 
-  it "renders attributes in <p>" do
+  it "Title is rendered" do
     render
+    expect(response.body).to match /Tokyo Cul Cul/im
   end
 end
