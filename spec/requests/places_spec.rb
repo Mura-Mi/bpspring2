273310bcffb9c158d2c21fc37ffe283da1d96e74
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2015-09-27 21:11:59 +0900

    Stop ignoring

--- spec/requests/places_spec.rb
+++ spec/requests/places_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 RSpec.describe "Places", type: :request do
   describe "GET /places" do
     it "works! (now write some real specs)" do
-      pending 'TODO Research'
       get places_path
       expect(response).to have_http_status(200)
     end
 
     it "works for getting new registration page" do
-      pending 'TODO Research'
       get new_place_path
       expect(response).to have_http_status(200)
     end
