d59b07b6d9ce4b83177b93661730706955cd8fdc
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2015-10-01 21:27:44 +0900

    Render sign up form in top page to fix #34

--- app/helpers/top_page_helper.rb
+++ app/helpers/top_page_helper.rb
@@ -1,2 +1,13 @@
 module TopPageHelper
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[resource_name]
+  end
 end
