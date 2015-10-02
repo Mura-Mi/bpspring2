35146c2075b26a26200144f808435860ca07281a
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2015-10-02 23:21:32 +0900

    Improve bootstrap customization
    Improve Yokohama Blue
    
    Add Hiroshima Red

--- db/fixtures/color_theme.rb
+++ db/fixtures/color_theme.rb
@@ -1,3 +1,4 @@
 ColorTheme.seed(:id,
-  {id: 1, theme_name: "Yokohama Blue", style_file_name: "application-db1"}
+  {id: 1, theme_name: "Yokohama Blue", style_file_name: "application-db1"},
+  {id: 2, theme_name: "Hiroshima Red", style_file_name: "application-c1"},
 )
