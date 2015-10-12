1aab47e780569232cd4f5e24f6e76336cd022153
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2015-10-12 23:09:51 +0900

    Show event reports in place view

--- app/helpers/event_reports_helper.rb
+++ app/helpers/event_reports_helper.rb
@@ -1,8 +1,8 @@
 module EventReportsHelper
-  def reports_grid(reports)
+  def reports_grid(reports, show_event_info = false)
     content_tag(:div, class: "grid bgh-event-reports-grid col-lg-12") do
         reports.collect { |report|
-          render(partial: 'event_reports/event_report', object: report)
+          render(partial: 'event_reports/event_report', object: report, locals: {show_event_info: show_event_info})
         }.join.html_safe
     end
   end
