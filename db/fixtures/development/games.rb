f66a15ed5258fc9424077579efe7061c0f92c89e
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2015-10-12 21:43:30 +0900

    Game fixture

---
+++ db/fixtures/development/games.rb
@@ -0,0 +1,16 @@
+Event.seed(:id,
+  {id: 101, name: "Swallows vs Carp", event_date: Date.new(2015, 9, 29), place_id: 6, abbreviation: 'S-C'},
+  {id: 102, name: "Swallows vs Tigers", event_date: Date.new(2015, 10, 1), place_id: 6, abbreviation: 'S-T'},
+  {id: 103, name: "Swallows vs Tigers", event_date: Date.new(2015, 10, 2), place_id: 6, abbreviation: 'S-T'},
+  {id: 104, name: "Giants vs Swallows", event_date: Date.new(2015, 10, 4), place_id: 1, abbreviation: 'G-S'},
+  {id: 105, name: "Tigers vs Baystars", event_date: Date.new(2015, 9, 29), place_id: 2, abbreviation: 'T-DB'},
+  {id: 106, name: "Baystars vs Giants", event_date: Date.new(2015, 10, 1), place_id: 5, abbreviation: 'DB-G'},
+  {id: 107, name: "Baystars vs Giants", event_date: Date.new(2015, 10, 2), place_id: 5, abbreviation: 'DB-G'},
+  {id: 108, name: "Baystars vs Giants", event_date: Date.new(2015, 10, 3), place_id: 5, abbreviation: 'DB-G'},
+  {id: 109, name: "Carp vs Dragons", event_date: Date.new(2015, 10, 1), place_id: 3, abbreviation: 'C-D'},
+  {id: 110, name: "Carp vs Dragons", event_date: Date.new(2015, 10, 2), place_id: 3, abbreviation: 'C-D'},
+  {id: 111, name: "Carp vs Swallows", event_date: Date.new(2015, 10, 3), place_id: 3, abbreviation: 'C-S'},
+  {id: 111, name: "Tibers vs Carp", event_date: Date.new(2015, 10, 4), place_id: 2, abbreviation: 'T-C'},
+  {id: 112, name: "Carp vs Dragons", event_date: Date.new(2015, 10, 7), place_id: 3, abbreviation: 'C-D'}
+
+)
