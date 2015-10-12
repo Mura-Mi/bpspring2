113a746468cdca475c3f15cf0539503078d40f36
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2015-10-12 22:10:27 +0900

    Externalize event report grid logic

--- app/helpers/event_reports_helper.rb
+++ app/helpers/event_reports_helper.rb
@@ -1,2 +1,9 @@
 module EventReportsHelper
+  def reports_grid(reports)
+    content_tag(:div, class: "grid bgh-event-reports-grid col-lg-12") do
+        reports.collect { |report|
+          render(partial: 'event_reports/event_report', object: report)
+        }.join.html_safe
+    end
+  end
 end
