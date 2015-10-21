7e2c738c4f8b596763b43f4db3811bc2a817ae36
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2015-10-22 01:22:41 +0900

    Fix Spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe HomeController, type: :controller do
   describe "GET #index" do
     it "returns http success when not logged in" do
       get :index
-      expect(response).to redirect_to(root_path)
+      expect(response).to redirect_to(top_page_index_path)
     end
   end
 
