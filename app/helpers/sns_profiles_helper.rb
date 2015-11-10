d4795655751ad98850afa933a663d762637bfe22
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2015-11-11 01:06:44 +0900

    Move SNS link button from partial view to helper

--- app/helpers/sns_profiles_helper.rb
+++ app/helpers/sns_profiles_helper.rb
@@ -1,4 +1,12 @@
 module SnsProfilesHelper
+  def profile_link_button(sns_profile)
+    divClass = "sns-link #{button_class(sns_profile)}"
+
+    link_to link(sns_profile), class: divClass, target: '_blank' do
+      content_tag :div, caption(sns_profile)
+    end
+  end
+
   def button_class(sns_profile)
     case sns_profile.provider_type
     when Providers::TWITTER
