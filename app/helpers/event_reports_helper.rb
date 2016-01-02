4bd2aac46239abd12999921a643f7eb67b2a5b0f
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-01-02 19:26:49 +0900

    Improve home for mobile

--- app/helpers/event_reports_helper.rb
+++ app/helpers/event_reports_helper.rb
@@ -1,6 +1,6 @@
 module EventReportsHelper
   def reports_grid(reports, show_event_info = false, additional_class = '')
-    content_tag(:div, class: "grid bgh-event-reports-grid col-lg-12") do
+    content_tag(:div, class: "grid bgh-event-reports-grid") do
         reports.collect { |report|
           render(partial: 'event_reports/event_report', object: report, locals: {show_event_info: show_event_info, additional_class: additional_class})
         }.join.html_safe
