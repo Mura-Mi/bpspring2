45b4890db2f22f760bfb3a9bcfc8ea83a76f98eb
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-01-05 00:49:37 +0900

    Adjust Masonry Grid

--- app/assets/javascripts/bgh-masonry.js
+++ app/assets/javascripts/bgh-masonry.js
@@ -1,6 +1,19 @@
+$('.grid').masonry(
+  {
+    itemSelector:    '.grid-item',
+    columnWidth:     '.grid-item',
+    percentPosition: true
+  }
+)
+
+
+
+/*
 $(function() {
   var grid = $('.grid');
   imagesLoaded(grid, function() {
     $('.grid').masonry();
   });
 });
+
+*/
