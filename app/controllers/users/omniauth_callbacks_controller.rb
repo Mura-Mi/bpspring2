25c4d2d230d08d5f0f8d7c17e897be97bd3a81f8
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-01-19 07:07:25 +0900

    Enable signup with Facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,4 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
-
   # You should also create an action method in this controller like this:
   def twitter
     auth_info = request.env['omniauth.auth']
@@ -14,6 +11,31 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         provider: auth_info['provider'],
         uid: auth_info['uid'],
         nickname: auth_info['info']['nickname'])
+
+      user = current_user || User.new(name: auth_info['info']['name'])
+      profile.user = user
+      user.sns_profile.push profile
+      profile.save
+      user.save
+    else
+      user = profile.user
+    end
+
+    sign_in(:user, user) unless current_user
+    redirect_to sign_up ? '/users/edit' : root_path ;
+  end
+
+  def facebook
+    auth_info = request.env['omniauth.auth']
+    profile = SnsProfile.find_by(provider: auth_info['provider'], uid: auth_info['uid'])
+
+    sign_up = !user_signed_in?
+
+    unless profile
+      profile = SnsProfile.new(
+        provider: auth_info['provider'],
+        uid: auth_info['uid'],
+        nickname: auth_info['info']['name'])
 
       user = current_user || User.new(name: auth_info['info']['name'])
       profile.user = user
