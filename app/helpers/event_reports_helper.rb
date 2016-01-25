044324619c62b4e318923c5f7fad04bd0532f4b7
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-01-26 08:03:13 +0900

    Adjust event report grid in home

--- app/helpers/event_reports_helper.rb
+++ app/helpers/event_reports_helper.rb
@@ -1,10 +1,8 @@
 module EventReportsHelper
   def reports_grid(reports, show_event_info = false, additional_class = '')
-    content_tag(:div, class: "grid bgh-event-reports-grid") do
-        reports.collect { |report|
-          render(partial: 'event_reports/event_report', object: report, locals: {show_event_info: show_event_info, additional_class: additional_class})
-        }.join.html_safe
-    end
+    reports.collect { |report|
+      render(partial: 'event_reports/event_report', object: report, locals: {show_event_info: show_event_info, additional_class: additional_class})
+    }.join.html_safe
   end
 
   def report_oneline_summary(report)
