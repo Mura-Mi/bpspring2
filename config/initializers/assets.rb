bbec3d88187f22486f605e9cfb4208adbfd62378
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-02-19 02:25:21 +0900

    add node to precompile path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 
 Rails.application.config.assets.precompile += ['base.css'];
+Rails.application.config.assets.precompile += ['node_modules'];
 
 Dir[Rails.root.join('app/assets/stylesheets/application-*.scss')].each do |f|
   Rails.application.config.assets.precompile += [File.basename(f).gsub(/scss$/, 'css')]
