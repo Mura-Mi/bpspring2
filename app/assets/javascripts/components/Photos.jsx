4a2bdc938e09c5bf07aa82049d73b182cd4d8fab
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-02-20 21:00:45 +0900

    Implement carousel in event photo

--- app/assets/javascripts/components/Photos.jsx
+++ app/assets/javascripts/components/Photos.jsx
@@ -1,9 +1,12 @@
 var Masonry = require('react-masonry-component')
-var Modal = require('react-bootstrap').Modal;
+var bootstrap = require('react-bootstrap');
+var Modal = bootstrap.Modal;
+var Carousel = bootstrap.Carousel;
+var CarouselItem = bootstrap.CarouselItem;
 
 var PhotoModal = React.createClass({
   getInitialState: function() {
-    return {open: false, title: 'No Title'};
+    return {open: false, title: 'No Title', index: 0};
   },
   onHide: function() {
     this.setState({open: false, title: 'No Title'});
@@ -13,16 +16,23 @@ var PhotoModal = React.createClass({
     var title = this.state.title || "No Title"
     return <Modal show={this.state.open} onHide={this.onHide}>
       <div className="modal-header"><h4>{title}</h4></div>
-      <div className='modal-body'><img src={src} className='img-responsive'></img></div>
+      <div className='modal-body'>
+        <Carousel defaultActiveIndex={this.state.index}>
+          {
+            this.props.photos.map(function(p) {
+              return <CarouselItem key={p.id}><img src={p.photo.url} className='img-responsive'/></CarouselItem>
+            })
+          }
+        </Carousel>
+      </div>
       <div className='modal-footer'><button className='btn btn-default' onClick={this.onHide}> Close </button></div>
     </Modal>
   }
 })
 
 var Photos = React.createClass({
-  handleClick: function(photo) {
-    this.refs.modal.setState({open: true, title: photo.title, img_url: photo.photo.url});
-    console.log('click!');
+  handleClick: function(photo, i) {
+    this.refs.modal.setState({open: true, title: photo.title, img_url: photo.photo.url, index: i});
   },
   render: function() {
     var op = {
@@ -37,15 +47,15 @@ var Photos = React.createClass({
         disableImagesLoaded={false}
       >
         <div className='grid-sizer'></div>
-        {this.props.data.map(function(photo) {
-          var boundClick = this.handleClick.bind(this, photo);
+        {this.props.data.map(function(photo, i) {
+          var boundClick = this.handleClick.bind(this, photo, i);
           return <div className='grid-item' key={photo.id}>
             <img src={photo.photo.mini.url} onClick={boundClick}
               className='img-responsive img-rounded first-photo center-block'/>
           </div>
         }, this)}
       </Masonry>
-      <PhotoModal ref='modal'></PhotoModal>
+      <PhotoModal ref='modal' photos={this.props.data}></PhotoModal>
     </div>
   }
 });
